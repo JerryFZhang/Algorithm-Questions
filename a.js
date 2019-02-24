1207a91ac4a256f9874238f6e1b5bf9be91d30e1
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-02-24 12:50:39 +0800

    added comments and wrapped the functions

--- a.js
+++ a.js
@@ -1,34 +1,31 @@
+// This is an online assessment challange demo provided by amazon.
 var states = [1, 1, 1, 0, 1, 1, 1, 1]
 var days = 2
-while (days) {
-    //    console.log(days + " day remaining")
-    var newState = []
-    states.unshift(0)
-    states.push(0)
-    states.forEach((item, index) => {
-        //        console.log(newState)
-        //        console.log(index + " item scanning")
-        //check if this is the first item
-        if (states[index - 1] === undefined) {
-            newState.push(0)
-        }
-        //check if this is the last item
-        else if (states[index + 1] === undefined) {
-            newState.push(0)
-        }
-        // if ((index !=0) && (index != states.length)){}
-        //both exists 
-        else if (states[index + 1] === states[index - 1]) {
-            //            console.log("this is a middle item " + item + " with " + states[index - 1] + " and " + states[index + 1] + " around." + " pushing 0") 
-            newState.push(0)
-        }
-        else {
-            //            console.log("this is a middle item " + item + " with " + states[index - 1] + " and " + states[index + 1] + " around." + " pushing 1") 
-            newState.push(1)
-        }
-    })
-    states = newState
-    states.splice(0, 1)
-    states.splice(states.length - 1, 1)
-    days--
+console.log(start(states, days))
+
+function start(states, days) {
+    while (days) {
+        // intiate next state 
+        var newState = []
+        // give padding to the existing array
+        states.unshift(0)
+        states.push(0)
+        // Loop throught the padded array and determine the next state
+        states.forEach((item, index) => {
+            // first item
+            if (states[index - 1] === undefined) newState.push(0)
+            // last item
+            else if (states[index + 1] === undefined) newState.push(0)
+            // middle item, if equal push 0 else push 1
+            else if (states[index + 1] === states[index - 1]) newState.push(0)
+            else newState.push(1)
+        })
+        states = newState
+        // remove padding
+        states.splice(0, 1)
+        states.splice(states.length - 1, 1)
+        // clock ticks
+        days--
+    }
+    return states
 }
