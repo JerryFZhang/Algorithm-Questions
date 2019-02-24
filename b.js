18acf87d18657c59909cb2d8a5ccba5e435bf12d
Jerry Fengwei Zhang <JerryFZhang@users.noreply.github.com>
2019-02-24 12:45:31 +0800

    added comments to b.js

--- b.js
+++ b.js
@@ -1,14 +1,29 @@
-const num = 5
-const arr = [2, 3, 4, 5, 6]
+// This is an online assessment challange demo provided by amazon.
+// The goal is to find a the greatest common denominator (GCD) for a given array and its size
 
+
+// Program input
+const num = 5
+const arr = [2, 3, 6]
+
+// Main 
 console.log(generalizedGCD(arr))
 
+// Recursive function
 function generalizedGCD(arr) {
+    // Counter for all divisible items
     var divisible = 0
+    
+    // If only one item, we are reaching the end of the array, return 1 
     if (arr.length == 1) return 1
+    
+    // Count all divisible items agains the first item in the array 
+
     for (var i = 1; i < arr.length; i++) {
         if (arr[i] % arr[0] == 0)  divisible ++
     }
+    // IF: the divisible counter returns the same value as the array length
+    // ELSE: keep looking for GCD
     if (divisible == arr.length - 1) return arr[0]
     else return generalizedGCD(arr.splice(arr.length - 1, 1))
 }
